0a44e62224e4ac339582707c30d8383d29f4d92b
YÄSÖ <jasmen.nasri2006@gmail.com>
2026-05-13 01:10:40 +0300

    set DEBUG False for production

--- kotopy/settings.py
+++ kotopy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ls*@*+l6@uobjbk)5$_f=1uu%ogdz+s-cgrf96@tatt2(gkp50'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 ALLOWED_HOSTS = ['*']
 
