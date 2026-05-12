f8381f1d1afbd4dbedf31e7d83ef10946d9a416a
YÄSÖ <jasmen.nasri2006@gmail.com>
2026-05-13 00:33:02 +0300

    fix static files and vercel config

--- kotopy/settings.py
+++ kotopy/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR /'core'/'static' ]
 
 MEDIA_URL = '/media/'
@@ -134,4 +134,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
