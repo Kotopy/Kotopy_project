f081610fa5d74316158289cca76a2750d3b588b7
YÄSÖ <jasmen.nasri2006@gmail.com>
2026-05-13 00:58:04 +0300

    fix vercel routing and settings

--- kotopy/settings.py
+++ kotopy/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
