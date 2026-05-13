22f52e5a34cfad37a01a7d97d5fd70d16e4b6da0
SaraElbably-2024 <saraelbably19@gmail.com>
2026-05-13 21:36:32 +0300

    Revert "Merge branch 'main' of https://github.com/SaraElbably-2024/Kotopy_project"
    This reverts commit 4d32381a8e2ef0c7c4140ce61e48878f145ddbe7, reversing
    changes made to 54a97f40e71ad08323a81b768dde1759e3f54f0e.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-#سمية علي
+
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile_view, name='edit_profile'),
