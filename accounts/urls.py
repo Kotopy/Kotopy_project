e1a656fe2c64c15a1fe0c29abd77fc968d1decb6
SaraElbably-2024 <saraelbably19@gmail.com>
2026-05-13 17:40:34 +0300

    fix: ignore pycache and clean up repository

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('profile/', views.profile_view, name='profile'),
-    path('profile/edit/', views.edit_profile_view, name='edit_profile'),
-    path('profile/change-password/', views.change_password_view, name='change_password'),
-]
