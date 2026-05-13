e1d16dcc361e3582d9b51da2e4f953a14eb28ed9
mohamedahmed203 <20242267@stud.fci-cu.edu.eg>
2026-05-13 17:14:25 +0300

    أول نسخة للمشروع

--- books/urls.py
+++ books/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('borrow/<int:book_id>/', views.borrow_book, name='borrow_book'),
     path('return/<int:book_id>/', views.return_book, name='return_book'),
     path('my-library/', views.my_library, name='my_library'),
+    path('search/', views.search_books, name='search_books'),
+     path('details/<int:id>/', views.book_details, name='book_details'),
+     path('books/', views.book_list, name='book_list'),
 ]
