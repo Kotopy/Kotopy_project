31a4c83ff28b8c7d19721658d72abbd38b3cdb15
mohamed <mmmelshimy@gmail.com>
2026-05-15 16:29:44 +0300

    solved signup

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,29 +1,26 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import NewSignupForm
 from django.contrib.auth.models import Group
 from django.shortcuts import render, redirect
 from accounts.models import Profile
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = NewSignupForm(request.POST)
         if form.is_valid():
             user = form.save()
 
             role = request.POST.get('role', 'user')
 
-            # Add to group
             group, _ = Group.objects.get_or_create(name=role)
             user.groups.add(group)
 
-            # Create Profile
             Profile.objects.get_or_create(user=user, defaults={'role': role})
 
-            # If admin, give is_staff
             if role == 'admin':
                 user.is_staff = True
                 user.save()
 
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = NewSignupForm()
     return render(request, 'auth/signup.html', {'form': form})
