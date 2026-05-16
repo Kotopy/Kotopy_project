710f31db985c3f62c54340207f6a02d82f2e48b6
SaraElbably-2024 <saraelbably19@gmail.com>
2026-05-16 18:36:35 +0300

    fix get started

--- kotopy/settings.py
+++ kotopy/settings.py
@@ -138,15 +138,13 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+ 
 
 # =========================
 # CLOUDINARY SETTINGS
